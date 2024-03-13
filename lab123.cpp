f76b071aa67f552edffeda5173c6ae66942b9927
NinaSidorenko <ndsidorenko@edu.hse.ru>
2024-03-13 08:49:53 +0300

    done

--- lab123.cpp
+++ lab123.cpp
@@ -5,12 +5,12 @@
 #include <string>
 #include <iostream>
 
-template <class Iter, typename Pred>
+template <class Iter, typename T>
 class FilterIterator
 {
         Iter pos;
         Iter end;
-        Pred pred;
+        std::function<bool(T)> pred;
 
         void plus()
         {
@@ -21,19 +21,33 @@ class FilterIterator
             }
         }
 
-    public:
-
-        typedef typename Iter::value_type value_type;
-        FilterIterator (): pos (NULL), end (NULL), pred (NULL) {}
-        FilterIterator (Iter pos, Iter end, Pred pred): pos (pos), end (end), pred (pred)
+        void check ()
         {
             while ((pos!=end) && (!pred(*pos)))
                 ++pos;
         }
+
+    public:
+
+        typedef typename Iter::value_type value_type;
+        FilterIterator (): pos (NULL), end (NULL), pred (NULL) {}
+        FilterIterator (Iter pos, Iter end, std::function<bool(T)> pred): pos (pos), end (end), pred (pred)
+        {
+            check();
+        }
         FilterIterator (const FilterIterator& other): pos (other.pos), end (other.end), pred (other.pred) {}
         ~FilterIterator() = default;
 
-        void set_pred (Pred pred) {this->pred = pred;}
+        void set_pred (std::function<bool(T)> pred) 
+        {
+            this->pred = pred;
+            check();          
+        }
+        void set_pos (Iter pos)
+        {
+            this->pos = pos;
+            check();
+        }
 
         value_type& operator * () const {return *pos;}
 
@@ -64,6 +78,11 @@ bool Equal (std::string elem)
    return elem == "Bye";
 }
 
+bool Equal1 (int elem)
+{
+   return elem == 6;
+}
+
 int main()
 {
     std::vector <int> v;
@@ -74,8 +93,21 @@ int main()
 
     std::function <bool (int)> ex1 = Even;
 
-    FilterIterator<std::vector<int>::iterator, std::function <bool (int)>> fit (v.begin(), v.end(), ex1);
-    FilterIterator<std::vector<int>::iterator, std::function <bool (int)>> fend (v.end(), v.end(), ex1);
+    FilterIterator<std::vector<int>::iterator, int> fit (v.begin(), v.end(), ex1);
+    FilterIterator<std::vector<int>::iterator, int> fend (v.end(), v.end(), ex1);
+
+    std::cout << *fit << std::endl;
+
+    for ( ; fit!=fend; ++fit)
+    {
+        std::cout << *fit << " ";
+    }
+    std::cout << std::endl;
+
+    std::function <bool (int)> ex3 = Equal1;
+    fit.set_pos(v.begin());
+    fit.set_pred(ex3);
+    fend.set_pred(ex3);
 
     for ( ; fit!=fend; ++fit)
     {
@@ -93,8 +125,8 @@ int main()
 
     std::function <bool (std::string)> ex2 = Equal;
 
-    FilterIterator<std::list<std::string>::iterator, std::function <bool (std::string)>> fit1 (l.begin(), l.end(), ex2);
-    FilterIterator<std::list<std::string>::iterator, std::function <bool (std::string)>> fend1 (l.end(), l.end(), ex2);
+    FilterIterator<std::list<std::string>::iterator, std::string> fit1 (l.begin(), l.end(), ex2);
+    FilterIterator<std::list<std::string>::iterator, std::string> fend1 (l.end(), l.end(), ex2);
 
     for ( ; fit1 != fend1; ++fit1)
     {
